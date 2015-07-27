9b37de21ee2d8a130b90bda9f76029cfc604b3f9
thea <tr@thearae.com>
2015-07-27 17:11:16 -0400

    working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
 
-before_action :logged_in_user, only: [:edit, :update]
+before_action :logged_in_user, only: [:index, :edit, :update]
 before_action :correct_user, only: [:edit, :update]
+
+  def index
+  @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
@@ -42,6 +46,7 @@ before_action :correct_user, only: [:edit, :update]
 
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "please log in"
         redirect_to login_url
       end
@@ -49,6 +54,6 @@ before_action :correct_user, only: [:edit, :update]
 
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless @user == current_user
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
